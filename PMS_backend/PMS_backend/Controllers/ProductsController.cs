3f431828dd34044eb1a3a3ceb484ae8910cd8043
Janani Wasana <janani96wasana@gmail.com>
2023-10-13 17:44:10 +0530

    Product controller

---
+++ PMS_backend/PMS_backend/Controllers/ProductsController.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using PMS_backend.Data;
+
+namespace PMS_backend.Controllers
+{
+    [Controller]
+    [Route("/api/[controller]")]
+    public class ProductsController : Controller
+    {
+        private readonly PMSDbContext _pmsDbContext;
+        public ProductsController(PMSDbContext pmsDbContext) 
+        {
+            this._pmsDbContext = pmsDbContext;
+        
+        }
+        [HttpGet]
+        public async Task<IActionResult> GetAllProducts()
+        {
+            var products = await _pmsDbContext.Products.ToArrayAsync();
+
+                return Ok(products);
+           
+        }
+    }
+}
