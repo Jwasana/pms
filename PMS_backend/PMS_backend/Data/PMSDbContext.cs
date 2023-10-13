d45e6ece16a3329a875902c440858a2830baff94
Janani Wasana <janani96wasana@gmail.com>
2023-10-13 17:43:34 +0530

    DB context

---
+++ PMS_backend/PMS_backend/Data/PMSDbContext.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+using PMS_backend.Models;
+
+namespace PMS_backend.Data
+{
+    public class PMSDbContext : DbContext
+    {
+        public PMSDbContext(DbContextOptions options) : base(options)
+        {
+        }
+
+        public DbSet<Product> Products { get; set; }
+
+    }
+}
